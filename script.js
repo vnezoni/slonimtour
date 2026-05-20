1e17aaa23cea60da15f675f17dd97b50d3436693
vnezoni <134688826+vnezoni@users.noreply.github.com>
2026-05-20 20:31:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 entry.target.classList.add('active');
             }
         });
-    }, { threshold: 0.1 });
+    }, { threshold: 0.2 });
     
     reveals.forEach(section => {
         observer.observe(section);
